55229e8864c734c12721c02577f6a3106e64c074
Goruchie <marcosgualtero43253@gmail.com>
2023-05-10 22:51:45 -0300

    solve step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -36,16 +36,26 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part1={props.part1} exercises1={props.exercises1}/>
+      <Part part2={props.part2} exercises2={props.exercises2}/>
+      <Part part3={props.part3} exercises3={props.exercises3}/>
     </div>
+  )
+}
+
+const Part = (props) => { 
+  return (
+    <>
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+    </>
   )
 }
 
